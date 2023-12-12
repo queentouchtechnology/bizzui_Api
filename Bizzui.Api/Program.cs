530b27af8bfdb549891c7b58b9dae6088a5260c3
leefordjudes <leefordjudes@gmail.com>
2023-12-12 22:14:46 +0530

    swagger ui added

--- Bizzui.Api/Program.cs
+++ Bizzui.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,15 +7,41 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Bizzui API",
+        Description = "An ASP.NET Core Web API for managing Bizzui",
+        TermsOfService = new Uri("https://bizzui.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Queentouch Technology",
+            Url = new Uri("https://queentouchtechnology.com/contact")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "License",
+            Url = new Uri("https://queentouchtechnology.com/license")
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwagger(options =>
+{
+    options.SerializeAsV2 = true;
+});
+    app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 }
 
 app.UseHttpsRedirection();
